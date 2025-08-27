f5403a3f7d326d8a87e079d075e9b121643346a4
Neel <neelp1025@gmail.com>
2025-08-26 21:36:03 -0400

    problem3 complexity

--- Problem3.java
+++ Problem3.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 //Time Complexity: O(nk) where n is the length of the words in the string since we have to iterate through each string to create the hash for each string with average k length
-//Space Complexity: O(n) where we store the mapping of each character to its mapped word
+//Space Complexity: O(n + m) where m is the number of words
 //Did this code successfully run on Leetcode : Yes
 
 /**
